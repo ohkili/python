304691f165632c61c0df9d86eef4fce57ab68768
ohkili <ohkili@daum.net>
2021-08-04 15:27:00 +0900

    인터파크 티켓 예매 구조를 거의 파악하였고 셀레니움으로 엘리먼트 찾고 수행하는 기능 정리 되었음

--- ticketing_interpark_for_macos.py
+++ ticketing_interpark_for_macos.py
@@ -7,37 +7,140 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.alert import Alert
-os = {'mac':'mac',
-      'win':'windows'}
+from bs4 import BeautifulSoup
 
-os_option = os['win']
-if os_option == 'mac':
-    executable_path = '/usr/local/bin/chromedriver' # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
-elif os_option == 'windows':
-    # executable_path = "C:\\Users\ohkil\\PycharmProjects\\chromedriver_win32\\chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
-    executable_path = "C:/Users\ohkil/PycharmProjects/chromedriver_win32/chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+# driver 초기화 하고 url 들어가는 함수
+def driverAct(url, option ='win'):
+    os = {'mac': 'mac',
+          'win': 'windows'}
 
-else:
-    print('Check your OS type')
+    os_option = os[option]
+    if os_option == 'mac':
+        executable_path = '/usr/local/bin/chromedriver'  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+    elif os_option == 'windows':
+        # executable_path = "C:\\Users\ohkil\\PycharmProjects\\chromedriver_win32\\chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+        executable_path = "C:/Users\ohkil/PycharmProjects/chromedriver_win32/chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+
+    else:
+        print('Check your OS type')
+    driver = webdriver.Chrome(executable_path=executable_path)
+    driver.set_window_size(1400, 1000)  # (가로, 세로)음
+    driver.get(url)
+    return driver
+
+# 예매 화면에서 관람일/회차선택으로 위치하게 하는 함수, 간혹 어뚱하게 좌석 선택 배송 선택등에 위치하는 경우가 있음
+def interparkTicketting_move_step1(driver):
+    # step_dic 'step info': 'main address','sub address', 'prev button', 'next button'
+    step_dic = {'step_1': ["//div[@class='contL']/iframe[@id='ifrmBookStep']",
+                           "//div[@class='contFrame frameBg6']"],
+                'step_2': ["//div[@id='divBookSeat']/iframe[@id='ifrmSeat']",
+                           "//div[@class='seatL']/iframe[@id='ifrmSeatDetail']",
+                           "//div[@class ='seatR']/div[@class='inner']/div[@class='btnWrap']/p[@class ='fl_l']/a"],
+                'step_3': ["//div[@class='contL']/iframe[@id='ifrmBookStep']",
+                           "//div[@class='contFrame frameBg2']",
+                           "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallPrevBtnLink']/img[@id='SmallPrevBtnImage']",
+                           "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallNextBtnLink']/img[@id='SmallNextBtnImage']"],
+                'step_4': ["//div[@class='contL']/iframe[@id='ifrmBookStep']",
+                           "//form[@id='formDelivery']",
+                           "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallPrevBtnLink']/img[@id='SmallPrevBtnImage']",
+                           "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallNextBtnLink']/img[@id='SmallNextBtnImage']"],
+                'step_5': ["//div[@class='contL']/iframe[@id='ifrmBookStep']",
+                           "//div[@class='contFrame frameBg3']",
+                           "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallPrevBtnLink']/img[@id='SmallPrevBtnImage']",
+                           "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallNextBtnLink']/img[@id='SmallNextBtnImage']"]
+                }
+    step_flag =5
+    driver.switch_to.default_content()
+    try:
+        #check step_5
+        driver.switch_to.default_content()
+        driver.switch_to.frame(driver.find_element(By.XPATH, step_dic['step_5'][0]))
+        driver.find_element(By.XPATH, step_dic['step_5'][1])
+        #return step_4
+        driver.switch_to.default_content()   # iframe 밖에 이전단계 버틑이 있어 iframe 밖으로 나와야 함
+        driver.find_element(By.XPATH, step_dic['step_5'][2]).click()
+
+    except NoSuchElementException:
+        print('NoSuchElementException')
+        print('Maybe your step is before step',step_flag)
+    step_flag -= 1
+    try:
+        # check step_4
+        driver.switch_to.default_content()
+        driver.switch_to.frame(driver.find_element(By.XPATH, step_dic['step_4'][0]))
+        driver.find_element(By.XPATH, step_dic['step_4'][1])
+        # retrun step_3
+        driver.switch_to.default_content()  # iframe 밖에 이전단계 버틑이 있어 iframe 밖으로 나와야 함
+        driver.find_element(By.XPATH, step_dic['step_4'][2]).click()
+    except NoSuchElementException:
+        print('NoSuchElementException')
+        print('Maybe your step is before step', step_flag)
+    step_flag -= 1
+    try:
+        # check step_3
+        driver.switch_to.default_content()
+        driver.switch_to.frame(driver.find_element(By.XPATH, step_dic['step_3'][0]))
+        driver.find_element(By.XPATH, step_dic['step_3'][1])
+        # return step2
+        driver.switch_to.default_content()  # iframe 밖에 이전단계 버틑이 있어 iframe 밖으로 나와야 함
+        driver.find_element(By.XPATH, step_dic['step_3'][2]).click()
+    except NoSuchElementException:
+        print('NoSuchElementException')
+        print('Maybe your step is before step', step_flag)
+    step_flag -=1
+
+    try:
+        # check step_2
+        driver.switch_to.default_content()
+        driver.switch_to.frame(driver.find_element(By.XPATH, step_dic['step_2'][0]))
+        driver.find_element(By.XPATH, step_dic['step_2'][1])
+        # return step_1
+        driver.find_element(By.XPATH, step_dic['step_2'][2]).click()  # iframe 안에서 실행하기 때문에 iframe 밖으로 나올 필요가 없다.
+    except NoSuchElementException:
+        print('NoSuchElementException')
+        print('Maybe your step is before step', step_flag)
+    step_flag -= 1
+
+    try:
+        # check step_1
+        driver.switch_to.default_content()
+        driver.switch_to.frame(driver.find_element(By.XPATH, step_dic['step_1'][0]))
+        driver.find_element(By.XPATH, step_dic['step_1'][1])  # 1 관람일/회차선택 메뉴 선택, 에러나면 해당 메뉴가 아니고 다른 메뉴임
+    except NoSuchElementException:
+        print('NoSuchElementException')
+        print('Maybe your step is before step', step_flag)
+    step_flag -=1
+
+    if step_flag<1:
+        print('Check your interpark reservation window, there is no match!')
+    else:
+        print('You are on step 1')
 
 def lack(t):
     print('sleep {0} sec'.format(t))
     time.sleep(t)
 
+def check_exists_by_element(by, name):
+    try:
+        driver.find_element(by, name)
+    except NoSuchElementException:
+        return False
+    return True
+
+
+
 # 예매조건
-wantYear = 2021
-wantMonth = 8
-wantDate = 19
-wantHour = 19
-wantMin = 0
+wantYear = '2021'
+wantMonth = '8'
+wantDate = '16'
+wantHour = '18'
+wantMin = '00'
 
+url = 'https://ticket.interpark.com/Gate/TPLogin.asp'
+driver = driverAct(url)
 
-
-
-driver = webdriver.Chrome(executable_path = executable_path)
 # driver.close()
 # 사이즈조절
 driver.set_window_size(1400, 1000)  # (가로, 세로)음
@@ -69,26 +172,7 @@ lack(2)
 # driver.switch_to.window(driver.window_handles[1])
 # driver.close()
 # 팝업창이 여러개 일때 사용하는데 210801 현재 팝업 인지도 안되고 팝업 제거도 안됨, driver.window_handles는 인터넷 tab을 인지함 , 팝업이 tab으로 인식되지 않음
-main = driver.window_handles
-for handle in main:
-    if handle != main[0]:   # main[0] means main
-        driver.switch_to.window(handle)
-        driver.close()
 
-
-try:
-    result = driver.switch_to.alert()
-    print(result.text)
-
-    # Alert 창 확인
-    result.accept()
-    # Alert 창 닫기
-    result.dismiss()
-
-except:
-    "popup : nothing"
-
-lack(2)
 
 
 # 예매안내 팝업 뜸 코로나 어쩌구 저쩌, 팝업 제거 안되면 예매하기 클릭 안됨
@@ -110,20 +194,56 @@ f.close()
 
 lack(2)
 
-# 예매하기 버튼임, 아래 구문을 보면 /a 다음에 속성 정보가 없는데 이렇게 하면 a tag 전체를 선택하게 되는것임
-driver.find_element(By.XPATH, "//div[@class='sideBtnWrap']/a").click() # 클릭까지 성공(210728)
-
-
-lack(2)
-
 # poticket.interpark.com 내용 제목으로 팝업 뜸, 관람일 전일 오후 어쩌구 저쩌구 취소/변경/환불 불가하다고 팝업 뜸 예매 진행하시겠습니까?
 # 라고 팝업 뜸
 # 이상태는 인터넷 탭이 하나 추가 된 상태임, 관람일 좌석 가격 선택하는 탭이 생성이 되었음 이제 해당 탭으로 옮겨 가야함
-# 그러나 소스는 메인에서 볼수 있음
+# 예매하기 누르기전에 날짜를 선택하는것으로 변경
+driver.switch_to.window(driver.window_handles[0])
+calen = driver.find_elements(By.CSS_SELECTOR, ".datepicker-panel")
+uls = calen[0].find_elements(By.TAG_NAME, "ul")
+year_month = uls[0].find_elements(By.TAG_NAME, "li")[1].text.split('. ')
+year = year_month[0]  # 년
+month = year_month[1]  # 월
 
-main = driver.window_handles
+yearC = int(wantYear) - int(year)
+monthC = int(wantMonth) - int(month)
 
-driver.switch_to.window(main[1])
+prev = uls[0].find_elements(By.TAG_NAME, "li")[0]
+next = uls[0].find_elements(By.TAG_NAME, "li")[2]
+
+s = yearC * 12 + monthC
+i = 0
+if s > 0:
+    while i < s:
+        next.click()
+        i = i + 1
+elif s < 0:
+    while i < s:
+        prev.click()
+        i = i - 1
+
+# 선택 가능한 날짜 모두 가져오기
+CellPlayDate =driver.find_elements(By.XPATH, "//ul[@data-view='days']/li[@class!='disabled']")
+for cell in CellPlayDate:
+    if cell.text == wantDate:
+        cell.click()
+        break
+
+    # 선택 가능한 시간 가져오기
+time_li = driver.find_elements(By.XPATH, "//a[@class='timeTableLabel']/span")
+
+hour_min = wantHour + ":" + wantMin
+
+for li in time_li:
+    if li.text == hour_min:
+        li.click()
+        break
+
+
+
+# 예매하기 버튼임, 아래 구문을 보면 /a 다음에 속성 정보가 없는데 이렇게 하면 a tag 전체를 선택하게 되는것임
+driver.find_element(By.XPATH, "//div[@class='sideBtnWrap']/a").click() # 클릭까지 성공(210728)
+
 
 
 # 파일 읽기
@@ -134,7 +254,20 @@ driver.switch_to.window(main[1])
 lack(2)
 
 
- # 예매안내 팝업 속성은 아래와 같으며 close 버튼 찾아서 눌러줌
+driver.window_handles
+driver.switch_to.window(driver.window_handles[1])
+
+
+
+# poticket.interpark.com 내용 제목으로 팝업 뜸, 관람일 전일 오후 어쩌구 저쩌구 취소/변경/환불 불가하다고 팝업 뜸 예매 진행하시겠습니까?
+# 라고 팝업 뜸
+# 이상태는 인터넷 탭이 하나 추가 된 상태임, 관람일 좌석 가격 선택하는 탭이 생성이 되었음 이제 해당 탭으로 옮겨 가야함
+# 예매하기 누르기전에 날짜를 선택하는것으로 변경 
+
+
+
+
+ # 예매안내 만 13세 어쩌구 저쩌구  팝업 속성은 아래와 같으며 close 버튼 찾아서 눌러줌
 driver.find_element(By.XPATH, "//div[(@class='bookNoticeLayer')]/div[@class='layerWrap']/div[@class='titleArea']/a[@class='closeBtn']").click()
 
 
@@ -150,143 +283,23 @@ f = open('E:/work/reserv_second_txt.txt','w', encoding='UTF-8')
 f.write(reserv_second_txt)
 f.close()
 
-# 날짜 지정하기
-# 날짜 아이프레임
-driver.switch_to.frame(driver.find_element(By.XPATH, "//div[@class='contL']/iframe[@id='ifrmBookStep']"))
+# 날짜는 예매하기 누르기 전에 선택하였으니 좌석 선택으로 넘어가자
+# step1로 이동하는 함수 어디에 위치해 있든 step1로 이동
+interparkTicketting_move_step1(driver)
 
-lack(2)
-
-# 월 체크
-calHead = driver.find_elements(By.XPATH, "//div[@class='calHead']/div[@class='month']/span")
-year_month = calHead[1].find_elements(By.XPATH, "//em")
-year = year_month[0].text  # 년
-month = year_month[1].text  # 월
-
-yearC = wantYear - int(year)	# wantYear : 예매 원하는 년
-monthC = wantMonth - int(month)	# wantMonth : 예매 원하는 월
-
-
-s = yearC * 12 + monthC
-i = 0
-# 월 이동
-if s > 0:
-    while i < s:
-        calHead[2].click() # cal head에서 span tag를 가져왔는데 0번째가 "prev", 1번째가 현재 년월, 2번째가 "next" 임 , calHead[2]를 선택하면 "next"를 선택하는 것임
-        time.sleep(2)
-        i = i + 1
-        calHead = driver.find_elements(By.XPATH, "//div[@class='calHead']/div[@class='month']/span")
-elif s < 0:
-    while i < s:
-        calHead[0].click()  # cal head에서 span tag를 가져왔는데 0번째가 "prev", 1번째가 현재 년월, 2번째가 "next" 임 , calHead[0]를 선택하면 "prev"를 선택하는 것임
-        i = i - 1
-        calHead = driver.find_elements(By.XPATH, "//div[@class='calHead']/div[@class='month']/span")
-
-
-
-# 선택 가능한 날짜 모두 불러오기
-CellPlayDate = driver.find_elements(By.NAME, "CellPlayDate") # 판매 가능한 날짜는 함수와 같이 조회하면 나오고, 판매 불가능한 날은 해당 id가 없다.
-
-print([cell.txt for cell in CellPlayDate])
-
-
-lack(2)
-
-
-# 일 선택
-for cell in CellPlayDate:
-    if cell.text == wantDate:	# wantDate : 예매 원하는 일
-        cell.click()
-        # time.sleep(2)
-        break
-
-lack(2)
-
+# 
 # 시간 클릭 전 활성화 대기
 # 태그가 만들어 질 때 까지 30초간 기다림
 # 30초전 태그가 활성화 되면 바로 실행
-#
-# time_li = WebDriverWait(driver, 30).until(
-#                 EC.presence_of_all_elements_located((By.ID, "CellPlaySeq"))
-#             )
-#
 
+# iframe의 부모 프레임으로 돌아가기
+driver.switch_to.default_content()
+# 다음버튼 클릭
+next = WebDriverWait(driver, 30).until(
+                EC.presence_of_all_elements_located((By.XPATH, "//div[@class='contR']/div[@class='buy_info']/p[@id='LargeNextBtn']/a/img"))
+            )
+next[0].click()
 
-# 현재 메뉴 상태가 어떤 상태인지 확인이 필요함
-# 이전 예매하던 이력이 있으면 1. 관람일/회차 선택이 아닌 2. 좌석 선택으로 넘어간 경우가 있어 이 부분을 체크할 필요가 있음
-# 체크후 메뉴가 맞지 않으면 원하는 곳으로 옮겨 줘야 함
-
-# 1 관람일 회차 선택 //div[@class='contL']/iframe[@id='ifrmBookStep] , //div[@class='contFrame frameBg6']
-# 1 다음단계 //div[@class='contL']/iframe[@id='ifrmBookStep] , //div[@class='contR']/div[@class='buy_info']/p[@id="LargeNextBtn"]/a
-
-# 2 좌석선택 tag 정보 //div[@id='divBookSeat']/iframe[@id='ifrmSeat']  , //div[@class='seatL']/iframe[@id='ifrmSeatDetail']
-# 2 이전단계  //div[@id="divBookSeat"]/iframe[@id="ifrmSeat"] , //div[@class='seatR']/div[@class='inner']/div[@class='btnWrap']/p[@class='fl_l']/a
-
-# 3 가격/할인 선택 tag정보  //div[@class='contL']/iframe[@id=ifrmBookStep], //div[@class='contFrame frameBg2'
-# 3 이전단계  "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallPrevBtnLink']/img[@id='SmallPrevBtnImage']"
-# 3 다음단계  "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallNextBtnLink']/img[@id='SmallNextBtnImage']"
-
-#4 배송선택/주문자확인 tag 정보 //div[@class='contL']/iframe[@id='ifrmBookStep] ,//div[@id='formDelivery']
-#4 이전단계  //p[@id="SmallNextBtn"]/a[@id="SmallPrevBtnLink"]
-#4 다음단계 //p[@id="SmallNextBtn"]/a[@id="SmallNextBtnLink"]
-
-#5 결제하기 tag 정보 //div[@class='contL']/iframe[@id='ifrmBookStep]  ,//div[@class='contFrame frameBg3']
-#5 이전단계 //p[@id="SmallNextBtn"]/a[@id="SmallPrevBtnLink"]
-#5 이후단계 //p[@id="SmallNextBtn"]/a[@id="SmallNextBtnLink"]
-# menu_dic 'step info': 'main address','sub address', 'prev button', 'next button'
-menu_dic = { 'step_1':["//div[@class='contL']/iframe[@id='ifrmBookStep']" ,"//div[@class='contFrame frameBg6']"],
-             'step_2':["//div[@id='divBookSeat']/iframe[@id='ifrmSeat']"  ,
-                       "//div[@class='seatL']/iframe[@id='ifrmSeatDetail']" ,
-                      "//div[@class ='seatR']/div[@class='inner']/div[@class='btnWrap']/p[@class ='fl_l']/a"],
-             'step_3':["//div[@class='contL']/iframe[@id='ifrmBookStep']",
-                       "//div[@class='contFrame frameBg2']",
-                       "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallPrevBtnLink']/img[@id='SmallPrevBtnImage']",
-                       "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallNextBtnLink']/img[@id='SmallNextBtnImage']"],
-             'step_4':["//div[@class='contL']/iframe[@id='ifrmBookStep']",
-                       "//form[@id='formDelivery']",
-                       "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallPrevBtnLink']/img[@id='SmallPrevBtnImage']",
-                       "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallNextBtnLink']/img[@id='SmallNextBtnImage']"],
-
-             'step_5':["//div[@class='contL']/iframe[@id='ifrmBookStep']",
-                       "//div[@class='contFrame frameBg3']" ,
-                       "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallPrevBtnLink']/img[@id='SmallPrevBtnImage']",
-                       "//div[@class ='contR']/div[@class ='buy_info']/p[@id='SmallNextBtn']/a[@id='SmallNextBtnLink']/img[@id='SmallNextBtnImage']"]
-             }
-
-driver.window_handles
-driver.switch_to.window(main[1])
-
-# 아래 구문을 함수로 만들어서 메뉴 오류가 생기지 않도록 할 예정임(210804 01:;40)
-
-# check step_1
-driver.switch_to.frame(driver.find_element(By.XPATH, menu_dic['step_1'][0]))
-driver.find_element(By.XPATH, menu_dic['step_1'][1]) # 1 관람일/회차선택 메뉴 선택, 에러나면 해당 메뉴가 아니고 다른 메뉴임
-
-# check step_2
-driver.switch_to.frame(driver.find_element(By.XPATH, menu_dic['step_2'][0]))
-driver.find_element(By.XPATH, menu_dic['step_2'][1])
-# return step_1
-driver.find_element(By.XPATH, menu_dic['step_2'][2]).click()  # iframe 안에서 실행하기 때문에 iframe 밖으로 나올 필요가 없다.
-
-#check step_3
-driver.switch_to.frame(driver.find_element(By.XPATH, menu_dic['step_3'][0]))
-driver.find_element(By.XPATH, menu_dic['step_3'][1])
-# return step2
-driver.switch_to.default_content()   # iframe 밖에 이전단계 버틑이 있어 iframe 밖으로 나와야 함
-driver.find_element(By.XPATH, menu_dic['step_3'][2]).click()
-
-#check step_4
-driver.switch_to.frame(driver.find_element(By.XPATH, menu_dic['step_4'][0]))
-driver.find_element(By.XPATH, menu_dic['step_4'][1])
-#retrun step_3
-driver.switch_to.default_content()   # iframe 밖에 이전단계 버틑이 있어 iframe 밖으로 나와야 함
-driver.find_element(By.XPATH, menu_dic['step_4'][2]).click()
-
-#check step_5
-driver.switch_to.frame(driver.find_element(By.XPATH, menu_dic['step_5'][0]))
-driver.find_element(By.XPATH, menu_dic['step_5'][1])
-#return step_4
-driver.switch_to.default_content()   # iframe 밖에 이전단계 버틑이 있어 iframe 밖으로 나와야 함
-driver.find_element(By.XPATH, menu_dic['step_5'][2]).click()
-
-
-
+# 자동예매 방지 문자열입력이 떠있는지 확인
+driver.switch_to.frame(driver.find_element(By.XPATH, "//div[@id='divBookSeat']/iframe[@id='ifrmSeat']"))
+capchaLayer_check = check_exists_by_element(By.XPATH, "//div[@id='divRecaptcha']")
