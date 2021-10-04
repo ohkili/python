55413a2781b90f7ded5e6601d1af3db5a2ce91f9
ohkili <ohkili@daum.net>
2021-10-05 00:33:32 +0900

    chrome driver를 자동으로 설치하여 사용할 수 있게 해주는 lib

--- chromedriver_autoinstall.py
+++ chromedriver_autoinstall.py
@@ -1,9 +1,16 @@
-from selenium import webdriver as wd
+from selenium import webdriver
 import chromedriver_autoinstaller
 import time
-path = chromedriver_autoinstaller.install()
-driver = wd.Chrome(path)
-driver.get("https://somjang.tistory.com")
-time.sleep(3.0)
-driver.close()
-driver.quit()
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]  #크롬드라이버 버전 확인
+
+try:
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')
+except:
+    chromedriver_autoinstaller.install(True)
+    driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')
+
+driver.implicitly_wait(10)
+driver.get("https://somjang.tistory.com") # 사용 샘플
+time.sleep(3.0) # 3초가 유지
+driver.close() # driver close close만 하면 driver가 살아 있음
+driver.quit() # driver 종료
