2ab8b733881eef77852c01a782506a6eddd7644d
ohkili <78248529+ohkili@users.noreply.github.com>
2022-05-22 23:13:56 +0900

    Update lotto_auto_purchase_for_macmini.py
    change driver act function

--- lotto_auto_purchase_for_macmini.py
+++ lotto_auto_purchase_for_macmini.py
@@ -109,28 +109,55 @@ def telegram_message(content='Hello world', content_type='text', description='de
         print('You must choice content_type as text, imgUrl, imgFile, hyperlink')
 
 # driver 초기화 하고 url 들어가는 함수, os가 윈도우인지, mac인지 구분하고 chrome driver가 저장된 위치를 명시하였으므로 개인 환경에 맞게 적절히 셋팅 필요
-def driverAct(url, option ='macmini'):
-    os = {'macmini': 'macmini',
-          'macpro' : 'macpro',
-          'win': 'windows'}
+# def driverAct(url, option ='macmini'):
+#     os = {'macmini': 'macmini',
+#           'macpro' : 'macpro',
+#           'win': 'windows'}
 
-    os_option = os[option]
-    if os_option == 'macmini':
-        executable_path =  '/Users/gwon-yonghwan/PycharmProjects/chromedriver'
-        #'/Users/home/PycharmProjects/chromedriver'   # '/usr/local/bin/chromedriver'  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
-    elif os_option == 'macpro':
-        executable_path = '/Users/home/PycharmProjects/chromedriver'
-    elif os_option == 'windows':
+#     os_option = os[option]
+#     if os_option == 'macmini':
+#         executable_path =  '/Users/gwon-yonghwan/PycharmProjects/chromedriver'
+#         #'/Users/home/PycharmProjects/chromedriver'   # '/usr/local/bin/chromedriver'  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+#     elif os_option == 'macpro':
+#         executable_path = '/Users/home/PycharmProjects/chromedriver'
+#     elif os_option == 'windows':
+#         # executable_path = "C:\\Users\ohkil\\PycharmProjects\\chromedriver_win32\\chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+#         executable_path = "C:/Users\ohkil/PycharmProjects/chromedriver_win32/chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+
+#     else:
+#         print('Check your OS type')
+#     driver = webdriver.Chrome(executable_path=executable_path)
+#     driver.set_window_size(1400, 1000)  # (가로, 세로)음
+#     driver.get(url)
+#     return driver
+
+def driverAct(url):
+    os_ver = platform.system()
+    plaform_ver = platform.platform()
+
+    if os_ver == 'Darwin' and plaform_ver == 'Darwin-19.6.0-x86_64-i386-64bit':
+        executable_path = '/Users/gwon-yonghwan/Downloads/chromedriver'
+        # '/Users/home/PycharmProjects/chromedriver'   # '/usr/local/bin/chromedriver'  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+        'driver activation for mac os'
+        driver = webdriver.Chrome(executable_path=executable_path)
+    elif os_ver == 'Darwin' and plaform_ver == 'macOS-10.16-x86_64-i386-64bit':
+        executable_path = '/Users/home/Downloads/chromedriver'
+        'driver activation for mac os'
+        driver = webdriver.Chrome(executable_path=executable_path)
+
+    elif os_ver == 'Windows' and plaform_ver.find('Windows') >= 0 :
+        # plaform_ver == 'Windows-10-10.0.19041-SP0'
         # executable_path = "C:\\Users\ohkil\\PycharmProjects\\chromedriver_win32\\chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
-        executable_path = "C:/Users\ohkil/PycharmProjects/chromedriver_win32/chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
-
+        # executable_path = "C:/Users\ohkil/PycharmProjects/chromedriver_win32/chromedriver.exe"  # 크롬드라이버가 보안에 막혀서 크롬드라이버를 압축풀고 해당 폴더로 이동시켜주었다
+        'driver activation for windows pc'
+        driver = chromedriver_autorun()
     else:
         print('Check your OS type')
-    driver = webdriver.Chrome(executable_path=executable_path)
+        telegram_message('Check your chrome driver path or version.')
+
     driver.set_window_size(1400, 1000)  # (가로, 세로)음
     driver.get(url)
     return driver
-
 
 def lack(t):
     print('sleep {0} sec'.format(t))
